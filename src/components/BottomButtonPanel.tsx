88e090371e1435be3e9cf36cf291f95e3aaedca9
Savien/Woo Jun Han <49388937+MrMirror21@users.noreply.github.com>
2025-02-24 21:56:43 +0900

    🐛 fix: 화면 길이에 상관없이 화면 하단에 버튼 고정 #1

--- src/components/BottomButtonPanel.tsx
+++ src/components/BottomButtonPanel.tsx
@@ -2,8 +2,10 @@ import { ReactNode } from 'react';
 
 const BottomButtonPanel = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="w-full absolute bottom-0 left-0 flex flex-row items-start justify-start px-4 py-3 box-border text-center subtle2-semibold z-10">
-      <div className="w-full flex items-center justify-center">{children}</div>
+    <div className="w-full fixed bottom-0 left-0 flex flex-row items-center justify-center py-3 box-border text-center subtle2-semibold z-10">
+      <div className="w-full max-w-[375px] px-5 flex items-center justify-center">
+        {children}
+      </div>
     </div>
   );
 };
