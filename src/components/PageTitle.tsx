182dfc62f1b3d28be210833518c22bd994dcd1a8
Savien/Woo Jun Han <49388937+MrMirror21@users.noreply.github.com>
2025-02-24 22:33:31 +0900

    🐛 fix: 헤더가 일부 ui 보다 아래로 가는 현상 수정 #1

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -9,13 +9,13 @@ type PageTitleProps = {
 const PageTitle = ({ onGoBack, title, subTitle }: PageTitleProps) => {
   return (
     <>
-      <header className="w-full pt-3 sticky top-0 bg-white">
+      <header className="w-full py-3 sticky top-0 bg-white z-20">
         <div className="w-5 h-5" onClick={onGoBack}>
           <BackIcon />
         </div>
       </header>
       {title && (
-        <section className="py-6 flex flex-col gap-2">
+        <section className="pt-3 pb-6 flex flex-col gap-2">
           <h1 className="head-h1 break-keep">{title}</h1>
           <p className="subtle2-regular text-text-secondary break-keep">
             {subTitle}
