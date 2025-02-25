02dcbfc4da1112aa17f59d602658b062bc787e92
Savien/Woo Jun Han <49388937+MrMirror21@users.noreply.github.com>
2025-02-25 19:48:01 +0900

    🎨 style: 모바일 화면 레이아웃 커버 위한 최대 수치 조정 #3

--- src/components/BottomButtonPanel.tsx
+++ src/components/BottomButtonPanel.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from 'react';
 const BottomButtonPanel = ({ children }: { children: ReactNode }) => {
   return (
     <div className="w-full fixed bottom-0 left-0 flex flex-row items-center justify-center py-3 box-border text-center subtle2-semibold z-10">
-      <div className="w-full max-w-[375px] px-5 flex items-center justify-center">
+      <div className="w-full max-w-[450px] px-5 flex items-center justify-center">
         {children}
       </div>
     </div>
